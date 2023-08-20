59caedebda6ac69520d3428f544ad241f4b04662
Candace Gostinski <candacegostinski@Candaces-MBP.lan>
2023-08-20 16:55:12 -0400

    added api functions

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -3,5 +3,26 @@ const { uuid } = require('uuidv4');
 const fs = require('fs');
 
 router.get('/api/notes', (req, res) => {
-    fs.readFileSync("db/db.json")
+    fs.readFile("db/db.json", "UTF-8", (err, data)=> {
+        const allNotes = data
+        res.json(JSON.parse(allNotes))
+    }) 
 });
+
+router.post('/api/notes', (req, res) => {
+    fs.readFile("db/db.json", "UTF-8", (err, data)=> {
+        const allNotes = JSON.parse(data)
+        const {title, text} = req.body;
+    const newNote = {
+        title, text, note_id: uuid()
+    }
+    allNotes.push(newNote)
+    fs.writeFile('db/db.json', JSON.stringify(allNotes), (err) => {
+        console.log("Note added!")
+        res.status(200).json({Message: "added note"})
+    })
+    }) 
+})
+
+
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -3,10 +3,15 @@ const path = require('path');
 const PORT = process.env.PORT || 3301;
 const app = express();
 const html_routes = require('./routes/html-routes.js');
+const api_routes = require('./routes/api-routes.js');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-
+app.use(api_routes);
 app.use(html_routes);
+
 
 
 
